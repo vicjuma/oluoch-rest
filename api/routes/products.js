aebc48ae972a4d89c420f1c5fce70d653a501d5d
= <vicjuma945@gmail.com>
2019-11-21 06:18:35 +0300

    add some products to the mongodb database and query it

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,10 +6,24 @@ const Product = require('./../../models/products');
 
 router.get('/', (req,res,next) => {
   Product.find()
+  .select('name price _id')
     .exec()
       .then((result) => {
-        console.log(result);
-        res.send(result);
+        const results = {
+          count: result.length,
+          products: result.map((resultt) => {
+            return {
+              name: resultt.name,
+              price: resultt.price,
+              id: resultt._id,
+              request: {
+                type:'GET',
+                url:`localhost:3000/products/${resultt._id}`
+              },
+            };
+          })
+        };
+        res.send(results);
       })
         .catch((err) => {
           console.error(err);
@@ -26,7 +40,15 @@ router.post('/', (req,res,next) => {
   product.save()
     .then((result) => {
       console.log(result);
-      res.send(result)
+      res.send({
+        name: result.name,
+        price: result.price,
+        id: result._id,
+        request: {
+          type: 'POST',
+          url: `localhost:3000/products/${result._id}`
+        }
+      })
     })
       .catch((err) =>{
         console.log(err);
@@ -36,32 +58,60 @@ router.post('/', (req,res,next) => {
 
 router.get('/:id', (req,res,next) => {
   const id = req.params.id
-  Product.findById({_id: id})
-  .exec()
-    .then((result) => {
-      if(mongoose.Types.ObjectId.isValid(id)){
-        res.send(result);
-      }else{
-        res.send('please input a valid id');
-      }
-    })
-      .catch((err) => {
-        console.error(err);
-      });
+  if(mongoose.Types.ObjectId.isValid(id)){
+    Product.findById({_id: id})
+    .select('name price _id')
+    .exec()
+      .then((result) => {
+        if(result){
+          res.send({
+            product: result,
+            request: {
+              type: 'GET',
+              desc: 'GET all products',
+              url: 'localhost:3000/products'
+            }
+          });
+        }else{
+          res.send('there is no entry');
+        }
+      })
+        .catch((err) => {
+          console.error(err);
+        });
+  }else{
+    res.status(400).json({error: 'please input a valid id'});
+  }
+ 
 });
 
 router.patch('/:id', (req,res,next) => {
-  const id = req.params.id
-  res.status(200).json({
-    message: `updated product with id ${id}`,
+  const id = req.params.id;
+  const product = new Product({
+    _id: id,
+    name: req.body.name,
+    price: req.body.price
   });
+
+  Product.update({_id: id}, {$set: product})
+    .then((result) => {
+      res.status(200).send(result);
+    })
+      .catch((error) => {
+        console.error(error);
+        res.send(error);
+      });
 });
 
 router.delete('/:id', (req,res,next) => {
   const id = req.params.id
-  res.status(200).json({
-    message: `deleted product with id ${id}`,
-  });
+  Product.deleteOne({_id: id})
+    .then((result) => {
+      res.status(201).send(result);
+    })
+      .catch((err) => {
+        console.error(err);
+      });
 });
 
 
